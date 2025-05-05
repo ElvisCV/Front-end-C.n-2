73ad695de112a35ff513554771f90f9d59be0faf
Elvis <elvis.candia@inacapmail.cl>
2025-05-05 16:13:19 -0400

    me falto un espacio xd

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.getElementById("studentForm").addEventListener("submit",function(e){
 
     const students=(name,LastName,grade)
     students.push(students);
-    addStudentToTable(student)
+     addStudentToTable(student)
     //console.log(students)
 
     this.reset();
